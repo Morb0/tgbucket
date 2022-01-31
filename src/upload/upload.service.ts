d90746f922242b0212e10e25053bcbdfc5f244e2
Morb0 <14136326+Morb0@users.noreply.github.com>
2022-01-31 22:44:57 +0300

    refactor(upload): change debug logging level & var rename

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -14,11 +14,11 @@ export class UploadService {
     private readonly filesService: FilesService,
   ) {}
 
-  async processFile(uploadFile: UploadFile): Promise<FileEntity> {
-    this.logger.debug('Upload file to Telegram');
+  async processFile(file: UploadFile): Promise<FileEntity> {
+    this.logger.log(`Uploading file to Telegram (${file.mimetype})`);
     const inputFile = await this.telegramService.uploadFile(
-      uploadFile.data,
-      uploadFile.size,
+      file.data,
+      file.size,
     );
 
     this.logger.debug('Send media message to Telegram');
@@ -29,16 +29,16 @@ export class UploadService {
     this.logger.debug('Save file info to database');
     const message = newMessageUpdate.message;
     const document = message.media.document;
-    const file = new FileEntity(
-      uploadFile.mimetype,
+    const fileEntity = new FileEntity(
+      file.mimetype,
       document.size,
       document.dc_id,
       document.id,
       document.access_hash,
       message.id,
     );
-    await this.filesService.create(file);
-    this.logger.debug('File successfully uploaded');
-    return file;
+    await this.filesService.create(fileEntity);
+    this.logger.log(`File "${fileEntity.id}" successfully uploaded`);
+    return fileEntity;
   }
 }
