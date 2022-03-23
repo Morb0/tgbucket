732bd99cb11fbdfc25a1d20fef67c9f84211adf3
Morb0 <14136326+Morb0@users.noreply.github.com>
2022-03-23 13:43:24 +0300

    feat: add auto migration on boot

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -4,6 +4,10 @@ export default {
   type: 'postgresql',
   clientUrl: process.env.DATABASE_URL,
   entitiesTs: ['./**/*.entity.ts'],
+  migrations: {
+    path: './migrations',
+    emit: 'ts',
+  },
   discovery: {
     warnWhenNoEntities: false,
   },
